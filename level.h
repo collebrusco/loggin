c795f4a99e6177d1cd445b77fd2d4e04a1708ede
collebrusco <collebrusco.frank@gmail.com>
2023-12-09 17:00:01 -0600

    first hack no test

---
+++ level.h
@@ -0,0 +1,8 @@
+// options:
+#define LOGGIN_NONE  0
+#define LOGGIN_ERROR 1
+#define LOGGIN_WARN  2
+#define LOGGIN_INFO  3
+#define LOGGIN_DEBUG 4
+
+#define _LOGGIN_LEVEL LOGGIN_DEBUG
--- loggin.h
+++ loggin.h
@@ -1,44 +1,73 @@
+
+#ifndef LOGGIN_H
+#define LOGGIN_H
+
 #ifdef __cplusplus
 extern "C" {
 #endif
+#include <cstdint.h>
+#include <stdio.h>
+#include <stdarg.h>
 
-
-#include "level_config.h"
-
-#if LOGGIN_LOG_LEVEL == LOGGIN_NONE
-	#define LOGGIN_MODULE_REGISTER(_NAME_) \
-		#define dpre "[[DEBUG]:_NAME_]: " \
-		#define ipre "[[INFO ]:_NAME_]: " \
-		#define wpre "[[WARN ]:_NAME_]: " \
-		#define epre "[[ERROR]:_NAME_]: " \
-		#include <stdio.h> \
-		#if LOGGIN_LOG_LEVEL >= LOGGIN_DEBUG \
-		    #define LOG_DBG(fmt, ...) printf(dpre fmt, ##__VA_ARGS__) \
-		#else \
-		    #define LOG_DBG(fmt, ...) \
-		#endif \
-		#if LOGGIN_LOG_LEVEL >= LOGGIN_INFO \
-		    #define LOG_INF(fmt, ...) printf(ipre fmt, ##__VA_ARGS__) \
-		#else \
-		    #define LOG_INF(fmt, ...) \
-		#endif \
-		#if LOGGIN_LOG_LEVEL >= LOGGIN_WARN \
-		    #define LOG_WARN(fmt, ...) printf(wpre fmt, ##__VA_ARGS__) \
-		#else \
-		    #define LOG_WARN(fmt, ...) \
-		#endif \
-		#if LOGGIN_LOG_LEVEL >= LOGGIN_ERROR \
-		    #define LOG_ERR(fmt, ...) printf(epre fmt, ##__VA_ARGS__) \
-		#else \
-		    #define LOG_ERR(fmt, ...) \
-		#endif \
-#else
-	#define LOGGIN_MODULE_REGISTER(name)
+#include "level.h"
+#ifndef _LOGGIN_LEVEL
+	#error "define log level in level.h"
 #endif
+
+#define LOGGIN_FILE_OPEN(name) \
+	#define _LOGGIN_FILE_OUT ; \
+	#undef _LOGGIN_OUT(level, name, fmt, ...) \
+	#define _LOGGIN_OUT(level, name, fmt, ...) fprintf(_logFile, "[[" level "] %s]: " fmt, name, ##__VA_ARGS__) \
+	FILE* _logFile = fopen(#name, "w")
+
+#define LOGGIN_FILE_CLOSE(name) fclose(_logFile)
+
+
+
+#ifdef _LOGGIN_FILE_OUT
+	extern FILE* _logFile;
+    #define _LOGGIN_OUT(level, name, fmt, ...) \
+        fprintf(_logFile, "[[" level "] %s]: " fmt, name, ##__VA_ARGS__)
+#else
+    #define _LOGGIN_OUT(level, name, fmt, ...) \
+        printf("[[" level "] %s]: " fmt, name, ##__VA_ARGS__)
+#endif
+
+#define LOGGIN_MODULE(name) \
+    static const char* moduleName = #name; \
+    \
+    #if LOG_LEVEL >= LOGGIN_DEBUG \
+        #define LOG_DBG(fmt, ...) _LOGGIN_OUT("DEBUG", moduleName, fmt, ##__VA_ARGS__) \
+    #else \
+        #define LOG_DBG(fmt, ...) \
+    #endif \
+    \
+    #if LOG_LEVEL >= LOGGIN_INFO \
+        #define LOG_INF(fmt, ...) _LOGGIN_OUT("INFO", moduleName, fmt, ##__VA_ARGS__) \
+    #else \
+        #define LOG_INF(fmt, ...) \
+    #endif \
+    \
+    #if LOG_LEVEL >= LOGGIN_WARN \
+        #define LOG_WRN(fmt, ...) _LOGGIN_OUT("WARN", moduleName, fmt, ##__VA_ARGS__) \
+    #else \
+        #define LOG_WRN(fmt, ...) \
+    #endif \
+    \
+    #if LOG_LEVEL >= LOGGIN_ERROR \
+        #define LOG_ERR(fmt, ...) _LOGGIN_OUT("ERROR", moduleName, fmt, ##__VA_ARGS__) \
+    #else \
+        #define LOG_ERR(fmt, ...) \
+    #endif
+
 
 
 
 #ifdef __cplusplus
 }
 #endif
+
+#endif // LOGGIN_H
+
+
 
