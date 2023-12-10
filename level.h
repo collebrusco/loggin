0a9d9de8a1250015d0b4f0a56ce645c5470e2f91
collebrusco <collebrusco.frank@gmail.com>
2023-12-10 02:42:24 -0600

    sorta works but sorta bad

--- level.h
+++
@@ -1,8 +0,0 @@
-// options:
-#define LOGGIN_NONE  0
-#define LOGGIN_ERROR 1
-#define LOGGIN_WARN  2
-#define LOGGIN_INFO  3
-#define LOGGIN_DEBUG 4
-
-#define _LOGGIN_LEVEL LOGGIN_DEBUG
--- loggin.h
+++ loggin.h
@@ -5,66 +5,76 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-#include <cstdint.h>
+#include <cstdint>
 #include <stdio.h>
 #include <stdarg.h>
 
-#include "level.h"
+#include "config.h"
 #ifndef _LOGGIN_LEVEL
-	#error "define log level in level.h"
+	#error "define log level & settings in config.h"
 #endif
 
-#define LOGGIN_FILE_OPEN(name) \
-	#define _LOGGIN_FILE_OUT ; \
-	#undef _LOGGIN_OUT(level, name, fmt, ...) \
-	#define _LOGGIN_OUT(level, name, fmt, ...) fprintf(_logFile, "[[" level "] %s]: " fmt, name, ##__VA_ARGS__) \
-	FILE* _logFile = fopen(#name, "w")
+FILE* _loggin_get_file();
+void loggin_file_close();
+#ifdef LOGGIN_FILE_IMPL
+	FILE* _loggin_get_file() {
+		static FILE* _logFile = fopen(_LOGGIN_FILE, "w");
+		return _logFile;
+	}
+	void loggin_file_close() {
+		fclose(_loggin_get_file());
+	}
+#endif
 
-#define LOGGIN_FILE_CLOSE(name) fclose(_logFile)
-
-
-
-#ifdef _LOGGIN_FILE_OUT
-	extern FILE* _logFile;
+#if _LOGGIN_USE_FILE == 1
     #define _LOGGIN_OUT(level, name, fmt, ...) \
-        fprintf(_logFile, "[[" level "] %s]: " fmt, name, ##__VA_ARGS__)
+        fprintf(_loggin_get_file(), "[(" level ") %s]: " fmt "\n", name, ##__VA_ARGS__)
 #else
     #define _LOGGIN_OUT(level, name, fmt, ...) \
-        printf("[[" level "] %s]: " fmt, name, ##__VA_ARGS__)
+        printf("[(" level ") %s]: " fmt "\n", name, ##__VA_ARGS__)
 #endif
 
-#define LOGGIN_MODULE(name) \
-    static const char* moduleName = #name; \
-    \
-    #if LOG_LEVEL >= LOGGIN_DEBUG \
-        #define LOG_DBG(fmt, ...) _LOGGIN_OUT("DEBUG", moduleName, fmt, ##__VA_ARGS__) \
-    #else \
-        #define LOG_DBG(fmt, ...) \
-    #endif \
-    \
-    #if LOG_LEVEL >= LOGGIN_INFO \
-        #define LOG_INF(fmt, ...) _LOGGIN_OUT("INFO", moduleName, fmt, ##__VA_ARGS__) \
-    #else \
-        #define LOG_INF(fmt, ...) \
-    #endif \
-    \
-    #if LOG_LEVEL >= LOGGIN_WARN \
-        #define LOG_WRN(fmt, ...) _LOGGIN_OUT("WARN", moduleName, fmt, ##__VA_ARGS__) \
-    #else \
-        #define LOG_WRN(fmt, ...) \
-    #endif \
-    \
-    #if LOG_LEVEL >= LOGGIN_ERROR \
-        #define LOG_ERR(fmt, ...) _LOGGIN_OUT("ERROR", moduleName, fmt, ##__VA_ARGS__) \
-    #else \
-        #define LOG_ERR(fmt, ...) \
-    #endif
+#define LOGGIN_MODULE(name) static const char* moduleName = #name
+
+#if _LOGGIN_LEVEL >= LOGGIN_DEBUG 
+    #define LOG_DBG(fmt, ...) _LOGGIN_OUT("DEBUG", moduleName, fmt, ##__VA_ARGS__) 
+#else 
+    #define LOG_DBG(fmt, ...) 
+#endif 
+
+#if _LOGGIN_LEVEL >= LOGGIN_INFO 
+    #define LOG_INF(fmt, ...) _LOGGIN_OUT("INFO ", moduleName, fmt, ##__VA_ARGS__) 
+#else 
+    #define LOG_INF(fmt, ...) 
+#endif 
+
+#if _LOGGIN_LEVEL >= LOGGIN_WARN 
+    #define LOG_WRN(fmt, ...) _LOGGIN_OUT("WARN ", moduleName, fmt, ##__VA_ARGS__) 
+#else 
+    #define LOG_WRN(fmt, ...) 
+#endif 
+
+#if _LOGGIN_LEVEL >= LOGGIN_ERROR 
+    #define LOG_ERR(fmt, ...) _LOGGIN_OUT("ERROR", moduleName, fmt, ##__VA_ARGS__) 
+#else 
+    #define LOG_ERR(fmt, ...) 
+#endif
 
 
 
 
 #ifdef __cplusplus
 }
+	#ifdef LOGGIN_FILE_IMPL
+	class _loggin_cpp {
+	public:
+		~_loggin_cpp() {
+			fclose(_loggin_get_file());
+			printf("closed file\n");
+		}
+	};
+	static _loggin_cpp _loggin_cpp_filemngr;
+	#endif
 #endif
 
 #endif // LOGGIN_H
